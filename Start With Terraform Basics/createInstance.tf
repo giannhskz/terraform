7d922bacdd237558d505cb2614268a803d1d3a8f
kazas <giannhskz@gmail.com>
2024-08-09 12:37:50 +0300

    change instance

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,10 +1,5 @@
 
 resource "aws_instance" "MyFirstInstnace" {
-  count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
-
-  tags = {
-    Name = "demoinstnce-${count.index}"
-  }
+  ami           = "	ami-0fd8f89f28610e525"
+  instance_type = "t2.micro"
 }
